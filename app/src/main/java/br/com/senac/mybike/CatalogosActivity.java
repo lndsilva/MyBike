0b8f733e1aa96c26bc045a870e9cacd6de23b972
lndsilva <lndsilva@hotmail.com>
2022-07-01 15:52:45 -0300

    criando o adaptador

--- app/src/main/java/br/com/senac/mybike/CatalogosActivity.java
+++ app/src/main/java/br/com/senac/mybike/CatalogosActivity.java
@@ -1,14 +1,32 @@
 package br.com.senac.mybike;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class CatalogosActivity extends AppCompatActivity {
+    RecyclerView idRecCatalogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.catalogos_layout);
+
+        idRecCatalogo = findViewById(R.id.idRecCatalogo);
+
+        //Montagem do layout que será utilizado na lista
+        idRecCatalogo.setLayoutManager(new GridLayoutManager(getApplicationContext(), 2));
+
+        //Vai deixar a lista mais eficiente
+        idRecCatalogo.hasFixedSize();
+
+        //Instanciar o adaptador
+        MyAdapter adapter = new MyAdapter();
+
+        //Executar a montagem da lista com o modelo
+        idRecCatalogo.setAdapter(adapter);
+
     }
 }
--- app/src/main/java/br/com/senac/mybike/ReservasActivity.java
+++ app/src/main/java/br/com/senac/mybike/ReservasActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ReservasActivity extends AppCompatActivity {
     FloatingActionButton fabReservar;
+    DatePicker dtpReservar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,7 @@ public class ReservasActivity extends AppCompatActivity {
         setContentView(R.layout.reservas_layout);
 
         fabReservar = findViewById(R.id.fabReservar);
+        dtpReservar = findViewById(R.id.dtpReservas);
 
         fabReservar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,7 +27,15 @@ public class ReservasActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),
                         "Reserva realizada!!!",
                         Toast.LENGTH_SHORT).show();
+
             }
         });
+
+
+        int mes = dtpReservar.getMonth();
+        int dia = dtpReservar.getDayOfMonth();
+        int ano = dtpReservar.getYear();
+
+        String montagem = "Você está no " + dia + "/" + mes + "/" + ano;
     }
 }
